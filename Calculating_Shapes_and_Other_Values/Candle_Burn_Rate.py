20a725f33397122617e14323733f0886d2eed7e0
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-04-29 11:15:29 -0700

    Update Candle_Burn_Rate.py

--- Calculating_Shapes_and_Other_Values/Candle_Burn_Rate.py
+++ Calculating_Shapes_and_Other_Values/Candle_Burn_Rate.py
@@ -42,7 +42,8 @@ while True:
     break
 
 #all values obtained by user input is calculated, rounded up, and printed for user
-#rate in grams/mins and how many total hours the candle can burn for
+#initial weight, time for candle burn, and final weight are from user input
+#total weight difference, rate of burn, total time in minutes, and total time in hours calculated from user input
 #these actions are done outside of any of the previous while loops
 print("")
 tweight = (iweight - fweight)
@@ -52,5 +53,6 @@ ttime = iweight / float(srate)
 hours = ttime / 60.0
 shours = Decimal(str(hours)).quantize(Decimal('.01'), rounding=ROUND_UP)
 
+#rate is in grams/mins and how many total hours the candle can burn for
 print("Ok! Your candle burns at a rate of about: " + str(srate) + " grams/minute")
 print("Your candle should be able to burn for approximately: " + str(shours) + " hours")
