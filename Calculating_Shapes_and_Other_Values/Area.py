a78bac2fa4af9d384e8c160bd2134eeefeb25b3f
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-04-29 11:23:09 -0700

    Update Area.py

--- Calculating_Shapes_and_Other_Values/Area.py
+++ Calculating_Shapes_and_Other_Values/Area.py
@@ -5,14 +5,14 @@ from math import pi
 print("Let's calculate area!\nPlease choose one of the following shapes:\nbox, sphere, or pyramid.")
 print("")
 
-#while loop continues until a given shape is chosen and a value > 0 is used
+#while loop continues until a given shape is chosen and a float value > 0 is used
 while True:
   try:
     shape = input("What shape is the object? ")
   except ValueError:
     continue
   else:
-#user input for 'box'
+#user can input 'box' in 7 different formats
     if shape in ("box", "BOX", "bOx", "BoX", "boX", "Box", "BOx"):
       while True:
         try:
@@ -41,10 +41,12 @@ while True:
           if height <= 0:
             continue
           break
-#formula for area of a box
+#user input of height, width, and length used
+#formula for area of a box is calulated and printed
       area = (2*(height * width)) + (2*(height * length)) + (2*(width * length))
       print("")
       print("The area of your box is: " + str(area) + " units^2")
+#user can input 'sphere' in 7 different formats
     elif shape in ("sphere", "SPHERE", "Sphere", "sPHERE", "SPhere", "SPHere", "sPhere"):
       while True:
         try:
@@ -55,11 +57,13 @@ while True:
           if radius <= 0:
             continue
           break
-#formula for area of a sphere
+#user input of radius is used
+#formula for area of a sphere is calculated, rounded to 0.1 of a decimal and printed
       area = 4 * pi * (radius ** 2)
       sarea = Decimal(str(area)).quantize(Decimal('.1'))
       print("")
       print("The area of your sphere is: " + str(sarea) + " units^2")
+#user can input 'pyramid' in 7 different formats
     elif shape in ("pyramid", "PYRAMID", "Pyramid", "pYRAMID", "PYramid", "PYRamid", "pYramid"):
       while True:
         try:
@@ -88,7 +92,8 @@ while True:
           if height <= 0:
             continue
           break
-#formula for area of a pyramid
+#user input of length, width, and height is used
+#formula for area of a pyramid is calculated, rounded to 0.1 of a decimal, and printed
       area = length * width + (length % (((width / 2)*(width / 2)) + (height*height))) + (width % (((length / 2)*(length / 2)) + (height*height)))
       sarea = Decimal(str(area)).quantize(Decimal('.1'))
       print("")
