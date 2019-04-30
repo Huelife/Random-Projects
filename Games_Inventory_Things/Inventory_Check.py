42ba3039292be862ad21dfa5757b613130b4f823
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-04-30 15:51:07 -0700

    Update Inventory_Check.py
    Adding comments

--- Games_Inventory_Things/Inventory_Check.py
+++ Games_Inventory_Things/Inventory_Check.py
@@ -1,6 +1,8 @@
 print("Let's check your inventory!")
 
+#setting items in inventory list
 inventory = ["sword","helmet","armor","shield","gun","boots","gloves"]
+#while loop continues until user inputs 'check' string
 while True:
   try:
     check = input("Type 'check' to check inventory.")
@@ -8,6 +10,7 @@ while True:
     continue
   else:
     if check == "check":
+#printing number of items in inventory list
       print("")
       print("You have "+str(len(inventory))+" items in your inventory.")
       print("Let's check what you have: "+str(inventory))
@@ -16,11 +19,14 @@ while True:
     else:
       continue
 print("You've picked up (3) more swords! Let's check how many swords you have now!")
+#creating add_sword function that adds 'sword' to inventory[1]
 def add_sword():
   inventory.insert(1, "sword")
+#three 'swords' are added to inventory list
 add_sword()
 add_sword()
 add_sword()
+#while loop continues until user inputs 'sword' string
 while True:
   try:
     sword_value = input("Type 'sword' to check the total number of swords in your inventory.")
@@ -28,8 +34,9 @@ while True:
     continue
   else:
     if sword_value == "sword":
+#entering 'sword' prints total number of 'swords' in inventory list
       print("")
-      print ("You have ("+str(inventory.count("sword"))+") swords")
+      print("You have ("+str(inventory.count("sword"))+") swords")
       break
     else:
       continue
