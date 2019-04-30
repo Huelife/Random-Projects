5ad6410a264864950193a326308811d23b5441bf
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-04-29 19:53:09 -0700

    Update Kg_to_Lbs.py
    Adding comments

--- Calculating_Workout_Values/Kg_to_Lbs.py
+++ Calculating_Workout_Values/Kg_to_Lbs.py
@@ -1,6 +1,7 @@
 print ("Let's convert pounds and kilograms!")
 print ("")
 
+#while loops until a float value > 0 is given
 while True:
   try:
     weight = float(input("How much weight would you like to convert? "))
@@ -11,16 +12,21 @@ while True:
       print("Please enter a value > 0")
       continue
     break
+#while loops until a string that is either lbs or kgs is chosen
 while True:
   try:
-    units = input("Do you want to convert to lbs or kgs? ")
+    units = input("Do you want to convert to 'lbs' or 'kgs'? ")
   except ValueError:
     continue
   else:
+#user input can be 4 different iterations of lbs
+#converting kgs value to lbs
     if units in("lbs","LBS","lb","LB"):
       solution = weight * 2.20
       unit1 = "kgs"
       unit2 = "lbs"
+#user input can be 4 different iterations of kgs
+#converting lbs value to kgs
     elif units in("kgs","KGS","kg","KG"):
       solution = weight / 0.45
       unit1 = "lbs"
@@ -28,5 +34,6 @@ while True:
     else:
       continue
     break
+#printing initial weight with units and converted weight with units
 print("")
 print("You converted " + str(weight) + " " + unit1 + " to " + str(solution) + " " + unit2)
