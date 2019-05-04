1430582a5074ce82d53c46feee681c37d471ee18
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-04 13:25:34 -0700

    Add files via upload

---
+++ Regular_Expression/User_Email_Input_Validation.py
@@ -0,0 +1,14 @@
+#simple user input email check
+
+import re
+
+user_email_check = r"[0-9a-z]+@[a-z]+\.[a-z]{2,3}"
+
+while True:
+  try:
+    user_email = input("Input a valid email. ")
+  except ValueError:
+    continue
+  else:
+    if re.search(user_email_check, user_email):
+      print("Match")
