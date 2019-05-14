874e325bf087a71011fe8ebc454585377644a4ec
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-13 23:14:18 -0700

    Update Inventory_Treasure_Chest_02.py

--- Games_Inventory_Things/Inventory_Treasure_Chest_02.py
+++ Games_Inventory_Things/Inventory_Treasure_Chest_02.py
@@ -3,15 +3,16 @@
 from random import randint
 
 #stats dictionary for 'Damage', 'Agility', and 'Speed' with values
-stats = {"Damage": [300, 2, 3],"Agility": [150, 5, 5],"Speed": [150, 5, 5],}
+stats_a = {"Damage:": [300, 2, 3],"Agility:": [150, 5, 5],"Speed:": [150, 5, 5],}
+stats_b = {"Damage:": [220, 2, 2],"Agility:": [190, 8, 8],"Speed:": [190, 8, 8],}
+stats_c = {"Damage:": [150, 1, 1],"Agility:": [300, 10, 10],"Speed:": [300, 10, 10],}
 
 pickup_random = 0
 
 #while loop continues until user input is 'no'
 while True:
   try:
-    print("Please choose 'yes' or 'no'.")
-    pickup = input("Do you want to open the treasure chest? ")
+    pickup = input("Please choose 'yes' or 'no'.\nDo you want to open the treasure chest? ")
   except ValueError:
     continue
   else:
@@ -20,27 +21,21 @@ while True:
       pickup_random = randint(0,10)
       if pickup_random in (0,1,4,6,8,9,10):
         print("_"*25)
-        print("Found: Sword A!")
-        for stats_column in stats:
-          print(" ",stats_column,stats[stats_column])
+        print("Found: Sword A!")      
+        for stats_column in stats_a:
+          print(" ",stats_column,stats_a[stats_column])
         print("_"*25)
       elif pickup_random in (2,5,7):
-        stats["Damage"] = [220, 2, 2]
-        stats["Agility"] = [190, 8, 8]
-        stats["Speed"] = [190, 8, 8]
         print("_"*25)
         print("Found: Sword B!")
-        for stats_column in stats:
-          print(" ",stats_column,stats[stats_column])
+        for stats_column in stats_b:
+          print(" ",stats_column,stats_b[stats_column])
         print("_"*25)
       elif pickup_random == 3:
-        stats["Damage"] = [150, 1, 1]
-        stats["Agility"] = [300, 10, 10]
-        stats["Speed"] = [300, 10, 10]
         print("_"*25)
         print("Found: Sword C!")
-        for stats_column in stats:
-          print(" ",stats_column,stats[stats_column])
+        for stats_column in stats_c:
+          print(" ",stats_column,stats_c[stats_column])
         print("_"*25)
     elif pickup == "no":
       break
