fcbdd32e1572619f3d7aff13a806d5c35447db54
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-15 19:57:25 -0700

    Update Inol_Single_02.py

--- Calculating_Workout_Values/Inol_Single_02.py
+++ Calculating_Workout_Values/Inol_Single_02.py
@@ -1,12 +1,11 @@
 #INOL stands for Intensity Number Of Lifts
 print("Let's figure out your INOL value!")
 print("")
-
 #while loop continues until a float value > 0 is entered
 #fluff code from lines 7-16, not used in the end and deletable
 while True:
   try:
-    weight = float(input("How much weight are you using?"))
+    weight = float(input("How much weight are you using? "))
   except ValueError:
     continue
   else:
@@ -17,7 +16,7 @@ while True:
 #while loop continues until a float value > 0, but less than 100, is entered
 while True:
   try:
-    perc = float(input("What % is this weight of your 1 rep max?"))
+    perc = float(input("What % is this weight of your 1 rep max? "))
   except ValueError:
     continue
   else:
@@ -28,7 +27,7 @@ while True:
 #while loop continues until an int value > 0 is entered
 while True:
   try:
-    sets = int(input("How many sets did you do?"))
+    sets = int(input("How many sets did you do? "))
   except ValueError:
     continue
   else:
@@ -39,7 +38,7 @@ while True:
 #while loop continues until an int value > 0 is entered
 while True:
   try:
-    reps = int(input("How many reps did you do?"))
+    reps = int(input("How many reps did you do? "))
   except ValueError:
     continue
   else:
@@ -47,12 +46,12 @@ while True:
       print("Reps should be greater than 0.")
       continue
     break
-#INOL is calculated with 2 different equations, one for: 0 < perc <= 99; and 99 < perc <= 100. No perc > 100 taken
+#INOL is calculated and rounded with 2 different equations, one for: 0 < perc <= 99; and 99 < perc <= 100. No perc > 100 taken
 #INOL requires user input for: weight, perc, sets, and reps. Weight value is not used in the current equations
 if perc > 0 and perc <= 99:
-  inol = (sets * reps) / (100.0 - perc)
+  inol = round(float((sets * reps) / (100.0 - perc)),3)
 elif perc > 99 and perc <= 100:
   inol = (sets * reps)
-#INOL value rounded and printed
+#INOL value printed
 print("")
-print("Your INOL is: " + str(round(float(inol),3)))
+print("Your INOL is: {}".format(inol))
