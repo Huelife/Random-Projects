07aed0f6c6e5a6ef85e743e2a650ec6ccad4fd7e
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-16 14:49:19 -0700

    Update Volume.py
    combined box and pyramid to reduce/optimize code length

--- Calculating_Shapes_and_Other_Values/Volume.py
+++ Calculating_Shapes_and_Other_Values/Volume.py
@@ -4,7 +4,8 @@ from math import pi
 
 print("Let's calculate volume! Please choose one of the following shapes:\nbox, sphere, or pyramid.")
 print("")
-
+box_tuple = ("box", "BOX", "bOx", "BoX", "boX", "Box", "BOx")
+pyramid_tuple = ("pyramid", "PYRAMID", "Pyramid", "pYRAMID", "PYramid", "PYRamid", "pYramid")
 #program loops until given shape is chosen and user inputs float value > 0
 while True:
   try:
@@ -13,10 +14,10 @@ while True:
     continue
   else:
 #user can input 'box' in 7 different formats
-    if shape in ("box", "BOX", "bOx", "BoX", "boX", "Box", "BOx"):
+    if shape in box_tuple or shape in pyramid_tuple:
       while True:
         try:
-          length = float(input("What's the length of the box? "))
+          length = float(input("What's the length of the {}? ".format(shape)))
         except ValueError:
           continue
         else:
@@ -25,7 +26,7 @@ while True:
           break
       while True:
         try:
-          width = float(input("What's the width of the box? "))
+          width = float(input("What's the width of the {}? ".format(shape)))
         except ValueError:
           continue
         else:
@@ -34,7 +35,7 @@ while True:
           break
       while True:
         try:
-          height = float(input("What's the height of the box? "))
+          height = float(input("What's the height of the {}? ".format(shape)))
         except ValueError:
           continue
         else:
@@ -42,9 +43,16 @@ while True:
             continue
           break
 #volume of a box is calculated and printed using user input values: length, width, and height
-      volume = length * width * height
-      print("")
-      print("The volume of your box is: " + str(volume) + " units^3")
+      if shape in box_tuple:
+        volume = length * width * height
+        print("")
+        print("The volume of your box is: " + str(volume) + " units^3")
+#volume of a pyramid is calculated and printed using user input value: length, width, and height. Rounding used to 0.1 of a decimal
+      elif shape in pyramid_tuple:
+        volume = (length * width * height) / 3
+        svolume = Decimal(str(volume)).quantize(Decimal('.1'))
+        print("")
+        print("The volume of your pyramid is: " + str(svolume) + " units^3")
 #user can input 'sphere' in 7 different formats
     elif shape in ("sphere", "SPHERE", "Sphere", "sPHERE", "SPhere", "SPHere", "sPhere"):
       while True:
@@ -61,40 +69,6 @@ while True:
       svolume = Decimal(str(volume)).quantize(Decimal('.1'))
       print("")
       print("The volume of your sphere is: " + str(svolume) + " units^3")
-#user can input 'pyramid' in 7 different formats
-    elif shape in ("pyramid", "PYRAMID", "Pyramid", "pYRAMID", "PYramid", "PYRamid", "pYramid"):
-      while True:
-        try:
-          length = float(input("What's the length of the pyramid? "))
-        except ValueError:
-          continue
-        else:
-          if length <= 0:
-            continue
-          break
-      while True:
-        try:
-          width = float(input("What's the width of the pyramid? "))
-        except ValueError:
-          continue
-        else:
-          if width <= 0:
-            continue
-          break
-      while True:
-        try:
-          height = float(input("What's the height of the pyramid? "))
-        except ValueError:
-          continue
-        else:
-          if height <= 0:
-            continue
-          break
-#volume of a pyramid is calculated and printed using user input value: length, width, and height. Rounding used to 0.1 of a decimal
-      volume = (length * width * height) / 3
-      svolume = Decimal(str(volume)).quantize(Decimal('.1'))
-      print("")
-      print("The volume of your pyramid is: " + str(svolume) + " units^3")
     else:
       continue
     break
