88a0690f7279b67b57321e0e338f9f28c0a2b6f9
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-17 20:31:58 -0700

    Update Surface_Area.py

--- Calculating_Shapes_and_Other_Values/Surface_Area.py
+++ Calculating_Shapes_and_Other_Values/Surface_Area.py
@@ -4,9 +4,11 @@ from math import pi
 
 print("Let's calculate surface area!\nPlease choose one of the following shapes:\n'box', 'sphere', or 'pyramid'.")
 print("")
+
 #possible inputs for box and pyramid
 shape_box = ("box", "BOX", "bOx", "BoX", "boX", "Box", "BOx")
 shape_pyramid = ("pyramid", "PYRAMID", "Pyramid", "pYRAMID", "PYramid", "PYRamid", "pYramid")
+
 #while loop continues until a given shape is chosen and a float value > 0 is used
 while True:
   try:
@@ -14,6 +16,7 @@ while True:
   except ValueError:
     continue
   else:
+    
 #user can input 'box' in 7 different formats and 'pyramid' in 7 different formats
     if shape in shape_box or shape in shape_pyramid:
       while True:
@@ -49,12 +52,14 @@ while True:
             print("Please input a value > 0.")
             continue
           break
+          
 #user input of height, width, and length used
 #formula for area of a box is calulated and printed
         if shape in shape_box:
           area = (2*(height * width)) + (2*(height * length)) + (2*(width * length))
           print("")
           print("The surface area of your {} is: {} units^2".format(shape,area))
+          
 #user input of length, width, and height is used
 #formula for area of a pyramid is calculated, rounded to 0.1 of a decimal, and printed
         elif shape in shape_pyramid:
@@ -62,6 +67,7 @@ while True:
           sarea = Decimal(str(area)).quantize(Decimal('.1'))
           print("")
           print("The surface area of your {} is: {} units^2".format(shape,sarea))
+          
 #user can input 'sphere' in 7 different formats
     elif shape in ("sphere", "SPHERE", "Sphere", "sPHERE", "SPhere", "SPHere", "sPhere"):
       while True:
@@ -75,6 +81,7 @@ while True:
             print("Please input a value > 0.")
             continue
           break
+          
 #user input of radius is used
 #formula for area of a sphere is calculated, rounded to 0.1 of a decimal and printed
       area = 4 * pi * (radius ** 2)
