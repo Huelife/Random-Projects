7e35e5770b67fa1dc89026b82ec42d68ab2da974
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-17 17:52:02 -0700

    Update Calculating_Known_Mpg.py

--- Calculating_Car_Values/Calculating_Known_Mpg.py
+++ Calculating_Car_Values/Calculating_Known_Mpg.py
@@ -3,7 +3,6 @@ from decimal import Decimal, ROUND_UP, ROUND_DOWN
 
 print("Let's calculate City/Highway MPG!")
 print("")
-
 #program loops until a float > 0 is entered
 while True:
   try:
@@ -23,6 +22,6 @@ while True:
     scity100 = Decimal(str(city100)).quantize(Decimal('.1'), rounding=ROUND_UP)
     shighway100 = Decimal(str(highway100)).quantize(Decimal('.1'), rounding=ROUND_DOWN)
 
-    print("If you drove 100% City, your mpg would be: " + str(scity100))
-    print("If you drove 100% Highway, your mpg would be: " + str(shighway100))
+    print("If you drove 100% City, your mpg would be: {}".format(scity100))
+    print("If you drove 100% Highway, your mpg would be: {}".format(shighway100))
     break
