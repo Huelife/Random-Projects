f36176a858005c58727fd659184faa29c016556d
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-17 20:34:16 -0700

    Update INOL_Single.py

--- Calculating_Workout_Values/INOL_Single.py
+++ Calculating_Workout_Values/INOL_Single.py
@@ -1,6 +1,7 @@
 #INOL stands for Intensity Number Of Lifts
 print("Let's figure out your INOL value!")
 print("")
+
 #while loop continues until a float value > 0 is entered
 #fluff code from lines 7-16, not used in the end and deletable
 while True:
@@ -14,6 +15,7 @@ while True:
       print("Weight should be greater than 0.")
       continue
     break
+    
 #while loop continues until a float value > 0, but less than 100, is entered
 while True:
   try:
@@ -26,6 +28,7 @@ while True:
       print("Percentage should be greater than 0, but less than or equal to 100.")
       continue
     break
+    
 #while loop continues until an int value > 0 is entered
 while True:
   try:
@@ -38,6 +41,7 @@ while True:
       print("Sets should be greater than 0.")
       continue
     break
+    
 #while loop continues until an int value > 0 is entered
 while True:
   try:
@@ -50,12 +54,14 @@ while True:
       print("Reps should be greater than 0.")
       continue
     break
+    
 #INOL is calculated and rounded with 2 different equations, one for: 0 < perc <= 99; and 99 < perc <= 100. No perc > 100 taken
 #INOL requires user input for: weight, perc, sets, and reps. Weight value is not used in the current equations
 if perc > 0 and perc <= 99:
   inol = round(float((sets * reps) / (100.0 - perc)),3)
 elif perc > 99 and perc <= 100:
   inol = (sets * reps)
+  
 #INOL value printed
 print("")
 print("Your INOL is: {}".format(inol))
