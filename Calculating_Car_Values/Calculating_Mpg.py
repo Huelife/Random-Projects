3b05e53a2c0ed43560b21a603b6ff9b2ff55bcbf
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-17 20:25:46 -0700

    Update Calculating_Mpg.py

--- Calculating_Car_Values/Calculating_Mpg.py
+++ Calculating_Car_Values/Calculating_Mpg.py
@@ -3,6 +3,7 @@ from decimal import Decimal, ROUND_UP, ROUND_DOWN
 
 print("Let's calculate City/Highway MPG!")
 print("")
+
 #gathering input from user for miles, gallons, and city percentage driving
 while True:
   try:
@@ -15,6 +16,7 @@ while True:
       print("Please enter a number > 0.")
       continue
     break
+    
 while True:
   try:
     gallons = float(input("How many gallons did you use? "))
@@ -26,6 +28,7 @@ while True:
       print("Please enter a number > 0.")
       continue
     break
+    
 while True:
   try:
     city = float(input("What percentage of city driving did you do? "))
@@ -37,7 +40,9 @@ while True:
       print("Please enter a number > 0.")
       continue
     break
+    
 print("")
+
 #calculating highway percentage, overall mpg, city mpg, and highway mpg and rounding them
 mpg = distance / gallons
 smpg = Decimal(str(mpg)).quantize(Decimal('.1'))
@@ -48,10 +53,12 @@ highway100 = mpg / 0.807
 
 scity100 = Decimal(str(city100)).quantize(Decimal('.1'), rounding=ROUND_UP)
 shighway100 = Decimal(str(highway100)).quantize(Decimal('.1'), rounding=ROUND_DOWN)
+
 #printing current mpg, and city and highway percentage
 print("Your mpg is: {}".format(smpg))
 print("You drove: {}% City/{}% Highway".format(city,highway))
 print("")
+
 #printing mpg if user drives 100% city or 100% highway
 print("If you drove 100% City, your mpg would be: {}".format(scity100))
 print("If you drove 100% Highway, your mpg would be: {}".format(shighway100))
