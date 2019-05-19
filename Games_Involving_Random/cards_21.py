399295c12468e131673314879cb87a49f8b72f34
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-19 14:27:21 -0700

    Update cards_21.py
    wrapped long lines and also fixed issue when multiple 'A's popped up, had add 3 additional cards_num list and changed it from a tuple

--- Games_Involving_Random/cards_21.py
+++ Games_Involving_Random/cards_21.py
@@ -12,7 +12,10 @@ print("--"*20)
 
 #value of cards and image of cards displayed
 cards_letters = (0,"A",2,3,4,5,6,7,8,9,10,"J","Q","K")
-cards_numbers = (0,1,2,3,4,5,6,7,8,9,10,10,10,10)
+cards_num = [0,1,2,3,4,5,6,7,8,9,10,10,10,10]
+cards_num2 = [0,1,2,3,4,5,6,7,8,9,10,10,10,10]
+cards_num3 = [0,1,2,3,4,5,6,7,8,9,10,10,10,10]
+cards_num4 = [0,1,2,3,4,5,6,7,8,9,10,10,10,10]
 
 #things I want to add later
 # if dealer = 16-21 stay
@@ -28,7 +31,7 @@ while True:
   except ValueError:
     continue
   else:
-#when user presses enter key, game begins and 4 random rolls from 1-13 are created
+#game begins with 4 random rolls from 1-13 when user presses enter
     if play == "":
       roll_one = randint(1,13)
       roll_two = randint(1,13)
@@ -37,29 +40,33 @@ while True:
            
  #setting value of 1 for user and dealer
       if roll_one == 1 and roll_two == 1:
-        cards_numbers[roll_two] = 11
-      elif roll_one == 1:
-        cards_numbers[roll_one] = 11
-      elif roll_two == 1:
-        cards_numbers[roll_two] = 11
-      elif dealer_roll_one == 1 and dealer_roll_two == 1:
-        cards_numbers[dealer_roll_two] = 11
-      elif dealer_roll_one == 1:
-        cards_numbers[dealer_roll_one] = 11
-      elif dealer_roll_two == 1:
-        cards_numbers[dealer_roll_two] = 11
+        cards_num[roll_one] = 11
+        cards_num2[roll_two] = 1        
+      elif roll_one == 1 and roll_two != 1:
+        cards_num[roll_one] = 11
+      elif roll_two == 1 and roll_one != 1:
+        cards_num2[roll_two] = 11
+      if dealer_roll_one == 1 and dealer_roll_two == 1:
+        cards_num3[dealer_roll_one] = 11
+        cards_num4[dealer_roll_two] = 1
+      elif dealer_roll_one == 1 and dealer_roll_two != 1:
+        cards_num3[dealer_roll_one] = 11
+      elif dealer_roll_two == 1 and dealer_roll_one != 1:
+        cards_num4[dealer_roll_two] = 11
 
 #setting total value of cards for dealer and user
-      your_total = cards_numbers[roll_one] + cards_numbers[roll_two]
-      dealer_total = cards_numbers[dealer_roll_one] + cards_numbers[dealer_roll_two]
+      your_total = cards_num[roll_one] + cards_num2[roll_two]
+      dealer_total = cards_num3[dealer_roll_one] + cards_num4[dealer_roll_two]
 
-#printing playing card images and total, using the created lists, based on the random numbers created for user and dealer
-      print("You: [{}][{}] = {}".format(cards_letters[roll_one],cards_letters[roll_two],your_total))
-      print("Dealer: [{}][{}] = {}".format(cards_letters[dealer_roll_one],cards_letters[dealer_roll_two],dealer_total))
+#printing playing card images and total, using the created lists
+      print("You: [{}][{}] = {}".format(cards_letters[roll_one],
+            cards_letters[roll_two],your_total))
+      print("Dealer: [{}][{}] = {}".format(cards_letters[dealer_roll_one],
+            cards_letters[dealer_roll_two],dealer_total))
       print("")
            
 #determining if user wins, wins with black jack, loses, or if it's a draw
-#user only gets 1 chance, unless it's a draw. Game repeats for draws until user wins or loses
+#Game repeats for draws until user wins or loses
       if your_total == 21 and dealer_total < 21:
         you += 1
       elif your_total == dealer_total:
@@ -75,7 +82,7 @@ while True:
       continue
     break
 
-#message that appears at the end to show if the user wins, wins with black jack, or loses
+#printing if user wins or loses
 if you >= 1 and your_total == 21:
   print("Black Jack! CONGRATULATIONS! YOUVE WON!")
 elif you >=1:
