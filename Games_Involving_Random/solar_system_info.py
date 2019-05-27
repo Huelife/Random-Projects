184df53a5b6dd456ca5681a138f0cb08fc60bc35
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-27 01:48:27 -0700

    Update solar_system_info.py
    removed unnecessary spaces in tuples and dictionary

--- Games_Involving_Random/solar_system_info.py
+++ Games_Involving_Random/solar_system_info.py
@@ -106,39 +106,39 @@ n10 = "n10"
 
 #planet tuples
 mercury_tuple = (me1,me2,me3,me4,me5,me6,me7,me8,me9,me10)
-mercury_tuple2 = ("mercury", "MERCURY", "Mercury", "MErcury", "me", "Me")
+mercury_tuple2 = ("mercury","MERCURY","Mercury","MErcury","me","Me")
 
 venus_tuple = (v1,v2,v3,v4,v5,v6,v7,v8,v9,v10)
-venus_tuple2 = ("venus", "VENUS", "Venus", "VEnus", "v", "V")
+venus_tuple2 = ("venus","VENUS","Venus","VEnus","v","V")
 
 earth_tuple = (e1,e2,e3,e4,e5,e6,e7,e8,e9,e10)
-earth_tuple2 = ("earth", "EARTH", "Earth", "EArth", "e", "E")
+earth_tuple2 = ("earth","EARTH","Earth","EArth","e","E")
 
 mars_tuple = (ma1,ma2,ma3,ma4,ma5,ma6,ma7,ma8,ma9,ma10)
-mars_tuple2 = ("mars", "MARS", "Mars", "MArs", "ma", "Ma")
+mars_tuple2 = ("mars","MARS","Mars","MArs","ma","Ma")
 
 jupiter_tuple = (j1,j2,j3,j4,j5,j6,j7,j8,j9,j10)
-jupiter_tuple2 = ("jupiter", "JUPITER", "Jupiter", "JUpiter", "j", "J")
+jupiter_tuple2 = ("jupiter","JUPITER","Jupiter","JUpiter","j","J")
 
 saturn_tuple = (s1,s2,s3,s4,s5,s6,s7,s8,s9,s10)
-saturn_tuple2 = ("saturn", "SATURN", "Saturn", "SAturn", "s", "S")
+saturn_tuple2 = ("saturn","SATURN","Saturn","SAturn","s","S")
 
 uranus_tuple = (u1,u2,u3,u4,u5,u6,u7,u8,u9,u10)
-uranus_tuple2 = ("uranus", "URANUS", "Uranus", "URanus", "u", "U")
+uranus_tuple2 = ("uranus","URANUS","Uranus","URanus","u","U")
 
 neptune_tuple = (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10)
-neptune_tuple2 = ("neptune", "NEPTUNE", "Neptune", "NEptune", "n", "N")
+neptune_tuple2 = ("neptune","NEPTUNE","Neptune","NEptune","n","N")
 
 #planet dictionary
 planet_dict = {
-  mercury_tuple2: mercury_tuple,
-  venus_tuple2: venus_tuple,
-  earth_tuple2: earth_tuple,
-  mars_tuple2: mars_tuple,
-  jupiter_tuple2: jupiter_tuple,
-  saturn_tuple2: saturn_tuple,
-  uranus_tuple2: uranus_tuple,
-  neptune_tuple2: neptune_tuple
+  mercury_tuple2:mercury_tuple,
+  venus_tuple2:venus_tuple,
+  earth_tuple2:earth_tuple,
+  mars_tuple2:mars_tuple,
+  jupiter_tuple2:jupiter_tuple,
+  saturn_tuple2:saturn_tuple,
+  uranus_tuple2:uranus_tuple,
+  neptune_tuple2:neptune_tuple
 }
 
 #while loop continues until user enters 'Q'
