c9233dff679c8c95043b58f45608ba38492f267b
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-28 15:02:41 -0700

    Update inventory_check_full.py
    rearranged order of stats for legendary sword to name, desc, stre, agi, spd, defe, vit, res, pie, rng

--- Games_Inventory_Things/inventory_check_full.py
+++ Games_Inventory_Things/inventory_check_full.py
@@ -25,8 +25,8 @@ bronze_sword = Item_Stats("Bronze Sword","A cheap, but effective sword for "
                           "combat.","+100","+50","+50",0,0,0,0,0)
 steel_sword = Item_Stats("Steel Sword","A strong, sturdy sword for combat.",
                          "+150","+100","+100",0,0,0,0,0)
-legendary_sword = Item_Stats("+300","+200","+200",0,0,0,0,0,"Legendary Sword",
-                             "A powerful sword for combat.")
+legendary_sword = Item_Stats("Legendary Sword","A powerful sword for combat.",
+                             "+300","+200","+200",0,0,0,0,0)
 
 bronze_helmet = Item_Stats(0,0,0,"+75","+75","+11",0,0,"Bronze Helmet",
                            "A cheap, but effective helmet for defense.")
