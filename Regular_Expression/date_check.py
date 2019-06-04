3bf52272bec0d8e54e4da26f51661f401056e889
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-06-04 15:22:41 -0700

    Update date_check.py
    added regular expression functioning code to check date and user input while loop

--- Regular_Expression/date_check.py
+++ Regular_Expression/date_check.py
@@ -1,1 +1,20 @@
 #date_check.py: Simple string date check
+
+import re
+
+#date check doesnt check if calender date exists
+date_check = r"[1-12]/[1-31]/[19-99]"
+
+#loop continues until user inputs a valid date
+while True:
+  try:
+    user_date = input("Input a valid date. ")
+  except ValueError:
+    continue
+  else:
+    if re.search(date_check,user_date):
+      print("Match!")
+      break
+    else:
+      print("Invalid.")
+      continue
