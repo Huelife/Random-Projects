f801958ecc1a44169aebf7bcbd8c874579d4b25d
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-06-04 17:08:36 -0700

    Update hr_check.py
    added necessary code for regular expression, with empty variable, user input, and input validation

--- Regular_Expression/hr_check.py
+++ Regular_Expression/hr_check.py
@@ -1,1 +1,19 @@
 #hr_check.py: Simple string hr check
+
+import re
+
+hr_check = r""
+
+#loop continues until user inputs a valid hour
+while True:
+  try:
+    user_hr = input("Input a valid hour. ex. '04:22PM' ")
+  except ValueError:
+    continue
+  else:
+    if re.search(hr_check,user_hr):
+      print("Match!")
+      break
+    else:
+      print("Invalid.")
+      continue
