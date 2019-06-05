6186d9a19614eac29fb49c3f51d6dd13886278f6
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-06-04 23:16:04 -0700

    Update date_check.py
    added more date_check variables because 00 days dont exist and added extra elif statements to compensate

--- Regular_Expression/date_check.py
+++ Regular_Expression/date_check.py
@@ -3,10 +3,12 @@
 import re
 
 #date check doesnt check if calender date exists
-date_check1 = r"0[1-9]/[0-2][0-9]/[1-9][0-9]"
-date_check2 = r"0[1-9]/3[0-1]/[1-9][0-9]"
-date_check3 = r"1[0-2]/[0-2][0-9]/[1-9][0-9]"
-date_check4 = r"1[0-2]/3[0-1]/[1-9][0-9]"
+date_check1 = r"0[1-9]/0[1-9]/[1-9][0-9]"
+date_check2 = r"0[1-9]/[1|2][0-9]/[1-9][0-9]"
+date_check3 = r"0[1-9]/3[0-1]/[1-9][0-9]"
+date_check4 = r"1[0-2]/0[1-9]/[1-9][0-9]"
+date_check5 = r"1[0-2]/[1|2][0-9]/[1-9][0-9]"
+date_check6 = r"1[0-2]/3[0-1]/[1-9][0-9]"
 
 #loop continues until user inputs a valid date
 while True:
@@ -22,6 +24,10 @@ while True:
     elif re.search(date_check3,user_date):
       print("Match!")
     elif re.search(date_check4,user_date):
+      print("Match!")
+    elif re.search(date_check5,user_date):
+      print("Match!")
+    elif re.search(date_check6,user_date):
       print("Match!")
     else:
       print("Invalid.")
