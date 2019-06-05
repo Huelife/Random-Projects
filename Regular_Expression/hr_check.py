8cad057887b184947ce5686cf10c4649c1067dbe
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-06-04 18:30:19 -0700

    Update hr_check.py
    filled in hr_check variable, created a second on, added extra check in the if statement and added a comment

--- Regular_Expression/hr_check.py
+++ Regular_Expression/hr_check.py
@@ -2,7 +2,9 @@
 
 import re
 
-hr_check = r""
+#checking 12 hr clock
+hr_check1 = r"0[0-9]:[0-5][0-9][AM|PM]"
+hr_check2 = r"1[0-2]:[0-5][0-9][AM|PM]"
 
 #loop continues until user inputs a valid hour
 while True:
@@ -11,7 +13,7 @@ while True:
   except ValueError:
     continue
   else:
-    if re.search(hr_check,user_hr):
+    if re.search(hr_check1,user_hr) or re.search(hr_check2,user_hr):
       print("Match!")
       break
     else:
